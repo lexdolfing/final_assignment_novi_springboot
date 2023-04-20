d3f763f16f282ff1e3f7f9a15ebc52b9f05f9f5c
lexdolfing <lexdolfing@gmail.com>
2023-04-20 10:31:06 +0200

    commit of project setup with Spring boot

---
+++ src/main/java/com/novi/DemoDrop/DemoDropApplication.java
@@ -0,0 +1,13 @@
+package com.novi.DemoDrop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoDropApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoDropApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/novi/DemoDrop/DemoDropApplicationTests.java
@@ -0,0 +1,13 @@
+package com.novi.DemoDrop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoDropApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
