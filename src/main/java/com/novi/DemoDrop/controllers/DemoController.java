5050459702ffdf65158440e2bfa8839d09ebf176
lexdolfing <lexdolfing@gmail.com>
2023-05-03 16:33:51 +0200

    added extra demo to data.sql

--- src/main/java/com/novi/DemoDrop/controllers/DemoController.java
+++ src/main/java/com/novi/DemoDrop/controllers/DemoController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@CrossOrigin
 @RequestMapping("demos")
 @RestController
 public class DemoController {
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,3 +1,7 @@
-INSERT INTO demos (id, email) VALUES (1, 'test@email.com');
+INSERT INTO demos (id, email, artist_name, song_name, song_elaboration)
+VALUES (1, 'test@email.com', 'DJ Dolfing', 'Electric slide', 'I was so inspired by old school house music');
+INSERT INTO demos (id, email, artist_name, song_name, song_elaboration)
+VALUES (2, 'test2@email.com', 'DJ Susanne', 'Electric slide double', 'I was so inspired by old school house music');
+
 INSERT INTO demo_replies (id, admin_comments, has_been_replied_to) VALUES (1, 'super leuk lied man', true);
 INSERT INTO dj (id, first_name, last_name, artist_name) VALUES (1, 'Lex', 'Dolfing', 'DJ Dolfing');
